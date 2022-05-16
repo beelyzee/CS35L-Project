f0978c2a0ea9cd1ce5721408830eba739930e445
Cameron Schofield <cameronschofield@Camerons-MacBook-Pro.local>
2022-05-15 20:51:30 -0700

    Fixed List.js category error

--- src/List.js
+++ src/List.js
@@ -1,35 +1,28 @@
 import * as React from "react";
 import getData from "./data.js";
-import getCategory from "./data.js";
+import {getCategory} from "./data.js";
 import Box from "@mui/material/Box";
-//import List from "@mui/material/List";
-//import ListItem from "@mui/material/ListItem";
-//import ListItemButton from "@mui/material/ListItemButton";
-//import ListItemText from "@mui/material/ListItemText";
 import Grid from "@mui/material/Grid";
-//import ListSubheader from "@mui/material/ListSubheader";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Collapse from "@mui/material/Collapse";
 
 export default function ThreeList(props) {
     
     const listElements = getData(props.username)[props.index];
     const listCategory = getCategory(props.index);
 
-    console.log(listElements);
-
+    console.log(listCategory);
+    
     const items: JSX.Element[] = [];
     for (let i = 0; i < listElements.length; i++) {
-	let PanelID = "panel" + (i+1) + "a";
 	items.push(
 	<Accordion>
             <AccordionSummary
 		expandIcon={<ExpandMoreIcon />}
-                aria-controls={PanelID + "-content"}
-                id={PanelID + "-header"}
+                aria-controls={"panel" + (i+1) + "a-content"}
+                id={"panel" + (i+1) + "a-header"}
             >
 		<header>{listElements[i]}</header>
             </AccordionSummary>

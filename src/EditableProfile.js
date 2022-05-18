5597d6fe1f5b30199931e88e74fa2623041013bf
Caden Thun <cadenthun@g.ucla.edu>
2022-05-18 15:26:48 -0700

    created basic profile page with basic edit button functionality

---
+++ src/EditableProfile.js
@@ -0,0 +1,62 @@
+import Edit from './Edit';
+import TextField from '@mui/material/TextField';
+
+function EditableProfile(props) {
+    return (
+        <div id='test'>
+            <Edit text={props.text} setText={props.setText}/>
+            <div className="edit-items">
+                <form name="TV Shows">
+                    <h1>TV Shows</h1>
+                    <TextField fullWidth placeholder='Show 1' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                    <br></br>
+                    <br></br>
+                    <TextField fullWidth placeholder='Show 2' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                    <br></br>
+                    <br></br>
+                    <TextField fullWidth type="text" placeholder='Show 3' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                </form>
+                <form name="Movies">
+                    <h1>Movies</h1>
+                    <TextField fullWidth placeholder='Movie 1' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                    <br></br>
+                    <br></br>
+                    <TextField fullWidth placeholder='Movie 2' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                    <br></br>
+                    <br></br>
+                    <TextField fullWidth placeholder='Movie 3' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                </form>
+                <form name="Songs">
+                    <h1>Songs</h1>
+                    <TextField fullWidth placeholder='Song 1' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                    <br></br>
+                    <br></br>
+                    <TextField fullWidth placeholder='Song 2' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                    <br></br>
+                    <br></br>
+                    <TextField fullWidth placeholder='Song 3' />
+                    <br></br>
+                    <TextField multiline fullWidth placeholder='Justification' />
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default EditableProfile;
--- src/Profile.js
+++ src/Profile.js
@@ -2,6 +2,7 @@ import ThreeList from './List';
 import Edit from './Edit';
 import { useState } from 'react';
 import './profile.css';
+import EditableProfile from './EditableProfile';
 
 function Profile() {
 
@@ -21,36 +22,8 @@ const [editText, setEditText] = useState("Edit");
     }
     else {
         return (
-            <div>
-                <Edit text={editText} setText={setEditText}/>
-                <div className="edit-items">
-                    <form name="TV Shows">
-                        <h1>TV Shows</h1>
-                        <input type="text" placeholder='Show 1' />
-                        <br></br>
-                        <input type="text" placeholder='Show 2' />
-                        <br></br>
-                        <input type="text" placeholder='Show 3' />
-                    </form>
-                    <form name="Movies">
-                        <h1>Movies</h1>
-                        <input type="text" placeholder='Movie 1' />
-                        <br></br>
-                        <input type="text" placeholder='Movie 2' />
-                        <br></br>
-                        <input type="text" placeholder='Movie 3' />
-                    </form>
-                    <form name="Songs">
-                        <h1>Songs</h1>
-                        <input type="text" placeholder='Song 1' />
-                        <br></br>
-                        <input type="text" placeholder='Song 2' />
-                        <br></br>
-                        <input type="text" placeholder='Song 3' />
-                    </form>
-                </div>
-            </div>)
-            ;
+            <EditableProfile text={editText} setText={setEditText} />
+            );
         
     }
 }

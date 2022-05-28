811df986ab2c09b5b3ff0da4daa3ca611d25469e
Cameron Schofield <cameronschofield@Camerons-MacBook-Pro.local>
2022-05-28 16:58:31 -0700

    Cleaning up data.js

--- src/data.js
+++ src/data.js
@@ -4,10 +4,6 @@ import { getCategories as getListOfCategories } from "./firebase-config.js";
 import { updateUserItemsData } from "./firebase-config.js";
 import { writeUserItemsData } from "./firebase-config.js";
 
-var usernames = [];
-var profileData = [];
-var categories = ["TV Shows", "Movies", "Songs"];
-
 // Return associated data to the username in profileData, or -1 if username is not found
 export default function getData(username, category) {
     return getUserItemsData(username, category);
@@ -35,13 +31,5 @@ export function getCategories() {
 
 // Return top ten (or less) ranked items in the category indicated by index
 export function getTop10(index) {
-    return profileData[0][index];
-}
 
-// Items is 2D array where each row are the recommendations for the corresponding category
-export function setData(username, items) {
-    usernames.push(username);
-    profileData.push(items);
 }
-
-setData("example-user", [ ["tv-1", "tv-2"], ["movie-1", "movie-2", "movie-3"], ["song-1"] ]);
